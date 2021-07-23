b9ff12b36644276c2533ea758194d4cfe12cf064
Mahmoud <mahmoud23199@gmail.com>
2021-07-23 20:55:52 +0200

    login_ui

---
+++ app/src/main/java/com/example/firstprojectv2/Sign_Up.kt
@@ -0,0 +1,11 @@
+package com.example.firstprojectv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_Up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
